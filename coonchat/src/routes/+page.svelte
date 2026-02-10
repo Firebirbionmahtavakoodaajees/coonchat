8c7706f97e115fc4922208c835325cbd39913af9
Firebirbionmahtavakoodaajees <kivasopo@outlook.com>
2026-02-10 13:12:25 +0200

    npx sv create coonchat

--- coonchat/src/routes/+page.svelte
+++ coonchat/src/routes/+page.svelte
@@ -1,1 +1,29 @@
-hii
+<script>
+    let user = "Nigger"
+</script>
+
+<div class="title-shell">
+    <h1 class="title">Welcome back {user}!</h1>
+</div>
+
+
+<style>
+.title-shell {
+    z-index: -1;
+    display: flex;
+    justify-content: center;
+    margin: 10dvh 10dvw 10dvh 10dvw;
+    border: 1px solid white;
+    border-radius: 10px;
+}
+.title {
+    font-family: "Anonymous Pro", monospace;
+    font-weight: 400;
+    font-style: normal;
+    color: white;
+}
+
+/*Desktop*/
+@media (max-width: 600px) {
+}
+</style>
