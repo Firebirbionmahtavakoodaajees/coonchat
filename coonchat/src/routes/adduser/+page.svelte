a73ffbf714ab3970abb63012ee5e7b51266b58b3
Firebirbionmahtavakoodaajees <kivasopo@outlook.com>
2026-02-12 12:09:21 +0200

    problem with padding

--- coonchat/src/routes/adduser/+page.svelte
+++ coonchat/src/routes/adduser/+page.svelte
@@ -28,7 +28,7 @@
 
 <div class="center-cardbox-container">
     <div class="card-viewport">
-        <div class="card-carousel" style="transform: translateX({-currentIndex * 220}px)">
+        <div class="card-carousel" style="transform: translateX({-currentIndex * 200}px)">
             {#each [0,1,2] as _}
                 <button class="center-cardbox"
                         title="Carrousel"
