5f0886e1abe2669d0936a9079c98ba356c2a2860
Firebirbionmahtavakoodaajees <kivasopo@outlook.com>
2026-02-17 20:08:43 +0200

    Document styling

--- coonchat/src/routes/login/+page.svelte
+++ coonchat/src/routes/login/+page.svelte
@@ -1,11 +1,9 @@
 <script>
     import { createClient } from '@supabase/supabase-js'
 
-
     const supabaseUrl = "https://dprudkvkotavzhieovfx.supabase.co"
     const supabaseKey = "sb_publishable_WGmi5uPdwEs8UwsNBrUNpQ_9SZb3W0d"
     const supabase = createClient(supabaseUrl, supabaseKey)
-
 
     let username = ""
     let password = ""
@@ -29,13 +27,11 @@
 <div class="username">
     Username:
 </div>
-
 <input bind:value={username}>
 
 <div class="username">
     Password:
 </div>
-
 <input bind:value={password}>
 
 <button on:click={login}>Submit</button>
